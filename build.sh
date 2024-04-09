741225ebfdab6908b2f9f211d108a3c98238fc58
Georg Lippitsch <georg@okrasolar.com>
2024-04-09 14:17:42 +0200

    Fix build.sh

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ set -e
 whoami
 pwd
 
-export TI_TOOLS_ROOT="/ti-cgt-armllvm_3.2.0.LTS"
+export TI_TOOLS_ROOT="/ti-cgt-armllvm_3.2.2.LTS"
 export TI_LIB="/simplelink_cc13xx_cc26xx_sdk_7_40_00_77"
 export TI_SYSCONFIG="/sysconfig_1.18.1"
 export TI_PROJECT_FOLDER_MODEM=$(pwd)
